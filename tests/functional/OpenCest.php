a349cb1b8f0389b0915062cd1a55330ce7d0ab8e
YuTanaka <YRK00337@nifty.com>
2016-11-03 17:41:43 +0900

    １つ目終わり

---
+++ tests/functional/OpenCest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class OpenCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    public function _after(FunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+        $I->amOnPage('/');
+        $I->see('Laravel');
+    }
+}
